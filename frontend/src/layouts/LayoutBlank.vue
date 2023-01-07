658c248e1e49621838a6cea199d3f7d717451eae
Max <dumerat.maxime@gmail.com>
2023-01-07 17:37:07 +0100

    add layout system

---
+++ frontend/src/layouts/LayoutBlank.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app :theme="myCustomLightTheme">
+    <v-main>
+      <slot></slot>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import { useTheme } from 'vuetify';
+
+export default {
+  setup() {
+    const theme = useTheme()
+    return {
+      theme,
+      toggleTheme: () => theme.global.name.value = theme.global.current.value.dark ? 'myCustomLightTheme' : 'dark'
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
