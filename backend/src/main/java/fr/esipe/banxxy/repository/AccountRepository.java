15205e16abab2562987830e14da16b8b912526f5
leo.buzelin <leo.buzelin@edu.univ-eiffel.fr>
2023-01-12 17:11:56 +0100

    remove: old entities

--- backend/src/main/java/fr/esipe/banxxy/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.esipe.banxxy.repository;
-
-import fr.esipe.banxxy.dao.Account;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AccountRepository extends CrudRepository<Account,String> {
-}
--- backend/src/main/java/fr/esipe/banxxy/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.esipe.banxxy.repository;
-
-import fr.esipe.banxxy.dao.Transaction;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TransactionRepository extends CrudRepository<Transaction, Long> {
-}
