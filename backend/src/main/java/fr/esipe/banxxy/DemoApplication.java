e5a509fdb7582019a59aea99ffbcbefd0e8a5d84
leo.buzelin <leo.buzelin@edu.univ-eiffel.fr>
2023-01-14 13:05:19 +0100

    fix: unused import

--- backend/src/main/java/fr/esipe/banxxy/DemoApplication.java
+++ backend/src/main/java/fr/esipe/banxxy/DemoApplication.java
@@ -2,8 +2,6 @@ package fr.esipe.banxxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class DemoApplication {
