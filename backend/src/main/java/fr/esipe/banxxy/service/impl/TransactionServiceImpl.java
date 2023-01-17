8ded123cada62a81646b6b6695a3279beca2d394
Robin <robin.rieutord@gmail.com>
2023-01-17 09:37:03 +0100

    feat : add transactionValidation

--- backend/src/main/java/fr/esipe/banxxy/service/impl/TransactionServiceImpl.java
+++ backend/src/main/java/fr/esipe/banxxy/service/impl/TransactionServiceImpl.java
@@ -1,6 +1,7 @@
 package fr.esipe.banxxy.service.impl;
 
 import fr.esipe.banxxy.dao.AdvisorEntity;
+import fr.esipe.banxxy.dao.TransactionEntity;
 import fr.esipe.banxxy.dao.UserEntity;
 import fr.esipe.banxxy.dto.TransactionDto;
 import fr.esipe.banxxy.repository.AccountRepository;
@@ -12,8 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.Query;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
@@ -37,9 +41,9 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public List<TransactionDto> getTransactionList(Integer accountId, Integer offset, Integer userId) {
-        var user = userRepository.findById(userId);
-        var account = accountRepository.findById(accountId);
-        var transactions = user.get().getTransactions().stream().filter(transaction -> transaction.getAccountFrom().equals(account) || transaction.getAccountTo().equals(account)).toList();
+        var user = userRepository.findById(userId).orElseThrow();
+        var account = accountRepository.findById(accountId).orElseThrow();
+        var transactions = user.getTransactions().stream().filter(transaction -> transaction.getAccountFrom().equals(account) || transaction.getAccountTo().equals(account)).toList();
 
        var transactionDtoList = new ArrayList<TransactionDto>();
        transactions.forEach(transaction ->{
@@ -59,6 +63,8 @@ public class TransactionServiceImpl implements TransactionService {
             return 0;
         var advisor = advisorRepository.findById(userId);
         var customers = advisor.get().getCustomers();
+        if (customers.isEmpty())
+            throw new NoSuchElementException("No customer found for this user");
         Long nbTransaction = 0L;
         for (var customer : customers) {
             nbTransaction += customer.getAccounts().stream()
@@ -67,11 +73,29 @@ public class TransactionServiceImpl implements TransactionService {
                     .reduce(0L, Long::sum);
         }
         return nbTransaction.intValue();
-
     }
 
     @Override
     public TransactionDto getTransactionValidation(Integer amount, Integer authorId, Integer account_fromId, Integer account_toId) {
-        return null;
+        var user = userRepository.findById(authorId).orElseThrow();
+        var accountFrom = accountRepository.findById(account_fromId).orElseThrow();
+        var accountTo  = accountRepository.findById(account_toId).orElseThrow();
+
+        var date = new Date();
+        if (accountFrom.getBalance() - amount < 0L){
+            return null;
+        }
+        var transactionEntity = new TransactionEntity();
+        transactionEntity.setAmount(amount);
+        transactionEntity.setAccountFrom(accountFrom);
+        transactionEntity.setAccountTo(accountTo);
+        transactionEntity.setDate(date);
+        transactionEntity.setAuthor(user);
+
+        accountFrom.setBalance(accountFrom.getBalance() - amount);
+        accountTo.setBalance(accountTo.getBalance() + amount);
+
+        return new TransactionDto(amount, user.getName(), account_fromId, account_toId, date.toString());
+
     }
 }
