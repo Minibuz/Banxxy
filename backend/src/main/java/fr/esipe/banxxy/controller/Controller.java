49734194ffd64d135454569350620e3b2db336ea
leo.buzelin <leo.buzelin@edu.univ-eiffel.fr>
2023-01-18 13:25:41 +0100

    fix: deepsource issues

--- backend/src/main/java/fr/esipe/banxxy/controller/Controller.java
+++ backend/src/main/java/fr/esipe/banxxy/controller/Controller.java
@@ -2,12 +2,13 @@ package fr.esipe.banxxy.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @org.springframework.stereotype.Controller
 public class Controller implements ErrorController {
     private static final String PATH = "/error";
 
-    @RequestMapping(value = PATH)
+    @RequestMapping(value = PATH, method = {RequestMethod.GET, RequestMethod.POST})
     public String error() {
         return "forward:/";
     }
