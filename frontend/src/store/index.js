f5c65f1ff58dfbb75a8add57f52603ef0b47b0cf
Max <dumerat.maxime@gmail.com>
2023-01-19 00:16:04 +0100

    fix : modification de la sauvegarde des informations sur l'utilisateur connecter coté front.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,35 +3,6 @@ import {createStore} from "vuex";
 import { auth } from "./auth.module";
 
 const store = createStore({
-    state () {
-        return {
-            count: 0,
-            auth: false,
-            user: null,
-            token: null,
-            role: null,
-            timeout: null
-        }
-    },
-    mutations: {
-        increment (state) {
-            state.count++
-        },
-        setAuth(state, data){
-            state.auth = true;
-            state.user = data.user;
-            state.token = data.token;
-            state.role = data.role;
-            state.timeout = Date.now();
-        },
-        resetAuth(state){
-            state.auth = false;
-            state.user = null;
-            state.token = null;
-            state.role = null;
-            state.timeout = null;
-        }
-    },
     modules: {
         auth,
     },
