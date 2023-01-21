12a6f4852b763ad1ab97e3d31fbc7e1b12298d84
Léo Barroux <leo.barroux@gmail.com>
2023-01-21 15:23:03 +0100

    feat: create and delete account

--- backend/src/main/java/fr/esipe/banxxy/controller/AccountController.java
+++ backend/src/main/java/fr/esipe/banxxy/controller/AccountController.java
@@ -1,14 +1,12 @@
 package fr.esipe.banxxy.controller;
 
+import fr.esipe.banxxy.dto.AccountDetailledDto;
 import fr.esipe.banxxy.dto.AccountDto;
 import fr.esipe.banxxy.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/account")
@@ -23,7 +21,25 @@ public class AccountController {
 
     @GetMapping("/{accountId}/{userId}")
     public ResponseEntity<AccountDto> getAccountDetails(@PathVariable Integer accountId, @PathVariable Integer userId) {
-        var accounts = accountService.getAccountDetails(accountId, userId);
-        return new ResponseEntity<>(accounts, HttpStatus.OK);
+        var opt = accountService.getAccountDetails(accountId, userId);
+        return opt.isEmpty() ?
+                new ResponseEntity<>(opt.get(), HttpStatus.BAD_REQUEST) :
+                new ResponseEntity<>(opt.get(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{userId}/{accountId}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable Integer userId, @PathVariable Integer accountId) {
+        var result = accountService.deleteAccount(userId, accountId);
+        return result ?
+                new ResponseEntity<>(HttpStatus.OK) :
+                new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<AccountDetailledDto> createAccount(@RequestBody AccountDetailledDto accountDto) {
+        var opt = accountService.createAccount(accountDto);
+        return opt.isEmpty() ?
+                new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED) :
+                new ResponseEntity<>(opt.get(), HttpStatus.CREATED);
     }
 }
--- backend/src/main/java/fr/esipe/banxxy/service/impl/AccountServiceImpl.java
+++ backend/src/main/java/fr/esipe/banxxy/service/impl/AccountServiceImpl.java
@@ -4,7 +4,9 @@ import fr.esipe.banxxy.dao.AccountEntity;
 import fr.esipe.banxxy.dao.AdvisorEntity;
 import fr.esipe.banxxy.dao.CustomerEntity;
 import fr.esipe.banxxy.dao.UserEntity;
+import fr.esipe.banxxy.dto.AccountDetailledDto;
 import fr.esipe.banxxy.dto.AccountDto;
+import fr.esipe.banxxy.repository.AccountRepository;
 import fr.esipe.banxxy.repository.AdvisorRepository;
 import fr.esipe.banxxy.repository.CustomerRepository;
 import fr.esipe.banxxy.repository.UserRepository;
@@ -13,8 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -23,18 +25,25 @@ public class AccountServiceImpl implements AccountService {
     private final UserRepository userRepository;
     private final AdvisorRepository advisorRepository;
     private final CustomerRepository customerRepository;
+    private final AccountRepository accountRepository;
 
     @Autowired
     public AccountServiceImpl(UserRepository userRepository,
                               AdvisorRepository advisorRepository,
-                              CustomerRepository customerRepository) {
+                              CustomerRepository customerRepository,
+                              AccountRepository accountRepository) {
         this.userRepository = userRepository;
         this.advisorRepository = advisorRepository;
         this.customerRepository = customerRepository;
+        this.accountRepository = accountRepository;
     }
 
     private boolean isAdvisor(UserEntity user) {
         return user instanceof AdvisorEntity;
+    }
+
+    private boolean isCustomer(UserEntity user) {
+        return user instanceof CustomerEntity;
     }
 
     private Set<CustomerEntity> getCustomersFromUser(UserEntity user, int id) {
@@ -47,30 +56,27 @@ public class AccountServiceImpl implements AccountService {
     }
 
     @Override
-    public AccountDto getAccountDetails(Integer accountId, Integer userId) {
+    public Optional<AccountDto> getAccountDetails(Integer accountId, Integer userId) {
+        var user = userRepository.findById(Integer.toUnsignedLong(userId));
+        if (!user.isPresent()) {
+            return Optional.empty();
+        }
+        var customers = getCustomersFromUser(user.get(), userId);
+        if (customers.isEmpty()) {
+            return Optional.empty();
+        }
         AccountEntity account;
         CustomerEntity customer;
-        System.out.println(userRepository.findAll());
-        System.out.println(advisorRepository.findAll());
-        System.out.println(customerRepository.findAll());
-        var user = userRepository.findById(Integer.toUnsignedLong(userId)).orElseThrow();
-        if (user instanceof AdvisorEntity)
-            // TODO - replace exception thrown by returning error to api
-            throw new IllegalArgumentException("Id should be a Customer, not an Advisor");
-        var customers = getCustomersFromUser(user, userId);
-        if (customers.isEmpty())
-            // TODO - replace exception thrown by returning error to api
-            throw new NoSuchElementException("No customer found for this user");
-        if (isAdvisor(user)) {
+        if (isAdvisor(user.get())) {
             account = customers.stream()
                     .map(CustomerEntity::getAccounts)
                     .flatMap(Collection::stream)
                     .filter(acc -> Objects.equals(acc.getId(), accountId.longValue()))
                     .findFirst()
                     .orElse(null);
-            if (account == null)
-                // TODO - replace exception thrown by returning error to api
-                throw new IllegalStateException("The account does not correspond to a Customer for this Advisor");
+            if (account == null) {
+                return Optional.empty();
+            }
             customer = account.getCustomer();
         } else {
             customer = customers.iterator().next();
@@ -78,16 +84,65 @@ public class AccountServiceImpl implements AccountService {
                     .filter(acc -> Objects.equals(acc.getId(), accountId.longValue()))
                     .findFirst()
                     .orElse(null);
-            if (account == null)
-                // TODO - replace exception thrown by returning error to api
-                throw new IllegalStateException("The account does not correspond to any of yours");
+            if (account == null) {
+                return Optional.empty();
+            }
         }
-        var advisor = customer.getAdvisor();
-        // TODO - change title when title exists in database
-        return new AccountDto(
+        return Optional.of(new AccountDto(
                 "title",
                 account.getId(),
                 account.getBalance()
-        );
+        ));
+    }
+
+
+    @Override
+    public boolean deleteAccount(Integer userId, Integer accountId) {
+        var opt = userRepository.findById(Long.valueOf(userId));
+        if (opt.isEmpty())
+            return false;
+        UserEntity user = opt.get();
+        if (isAdvisor(user)) {
+            AdvisorEntity advisor = advisorRepository.findById(Long.valueOf(userId)).orElse(null);
+            if (advisor == null)
+                return false;
+            Set<CustomerEntity> customers = advisor.getCustomers();
+            for (CustomerEntity customer : customers) {
+                if (customer.getAccounts().removeIf(account -> account.getId().equals(Long.valueOf(accountId))))
+                    return true;
+            }
+        } else if (isCustomer(user)) {
+            CustomerEntity customer = customerRepository.findById(Long.valueOf(userId)).orElse(null);
+            if (customer == null)
+                return false;
+            return customer.getAccounts().removeIf(account -> account.getId().equals(Long.valueOf(accountId)));
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<AccountDetailledDto> createAccount(AccountDetailledDto accountDto) {
+        var opt = customerRepository.findById(accountDto.getId_owner());
+        if (opt.isEmpty())
+            return Optional.empty();
+        System.out.println("ARGH = " + opt.get());
+        CustomerEntity customer = opt.get();
+        AccountEntity account = new AccountEntity();
+        account.setCustomer(customer);
+        account.setBalance(accountDto.getBalance());
+        account = accountRepository.save(account);
+        AdvisorEntity advisor = advisorRepository.findByCustomersContaining(customer);
+        String advisorFirstName = advisor.getFirstname();
+        String advisorLastName = advisor.getName();
+        return Optional.of(new AccountDetailledDto(
+                account.getId(),
+                accountDto.getTitle(),
+                customer.getFirstname(),
+                customer.getName(),
+                advisorFirstName,
+                advisorLastName,
+                account.getBalance(),
+                customer.getId()
+        ));
     }
 }
