f000137d815ab40d13c682bd39be29267ce94fc1
martin <martin.ricard@edu.univ-eiffel.com>
2023-01-26 22:54:55 +0100

    feat : documentation of AdvisorEntity

--- backend/src/main/java/fr/esipe/banxxy/dao/AdvisorEntity.java
+++ backend/src/main/java/fr/esipe/banxxy/dao/AdvisorEntity.java
@@ -11,10 +11,20 @@ public class AdvisorEntity extends UserEntity {
     @OneToMany(mappedBy = "advisor", fetch = FetchType.EAGER)
     private Set<CustomerEntity> customers;
 
+    /**
+     * Returns a set of this advisor's customers.
+     *
+     * @return customers A set of this advisor's customers. (Set<CustomerEntity>)
+     */
     public Set<CustomerEntity> getCustomers() {
         return customers;
     }
 
+    /**
+     * Sets a new set of customers for this advisor.
+     *
+     * @param customers A new set of customers for this advisor. (Set<CustomerEntity>)
+     */
     public void setCustomers(Set<CustomerEntity> customers) {
         this.customers = customers;
     }
